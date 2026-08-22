f53210259bf980489c17ff10a2626b3eb45785f6
Peifan Li <frank.li.oxygen@gmail.com>
2026-08-22 10:52:52 -0400

    chore(release): v0.3.35

--- Sources/V2SApp/App/AppModel.swift
+++ Sources/V2SApp/App/AppModel.swift
@@ -6,8 +6,8 @@ import SwiftUI
 import Translation
 
 private enum AppBuildInfo {
-    static let marketingVersion = "0.3.34"
-    static let buildNumber = "38"
+    static let marketingVersion = "0.3.35"
+    static let buildNumber = "39"
     static let repositoryURLString = "https://github.com/franklioxygen/v2s"
     static let repositoryURL = URL(string: repositoryURLString)
 }
